6cc658c4b99d4e2d367fd7390a8eba2a7c886df2
Alex <ajs476@nau.edu>
2016-04-10 16:05:59 -0700

    updated main screen buttons and logo

--- OnTap/app/src/main/java/cs477/ontap/MainActivity.java
+++ OnTap/app/src/main/java/cs477/ontap/MainActivity.java
@@ -2,6 +2,9 @@ package cs477.ontap;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginButton = (Button)findViewById(R.id.button_mainLogin);
+        Button registerButton = (Button)findViewById(R.id.button_mainRegister);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Login Dialogue Popup HERE", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "GOTO account creation page 1", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
