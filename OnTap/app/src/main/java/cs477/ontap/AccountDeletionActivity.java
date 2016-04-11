411da4a4cfe74ea975df019217a3dc8169b28c3a
DougP723 <dougpeterson23@gmail.com>
2016-04-10 22:42:58 -0700

    Added 4 Screens
    Account Management
    Update Account Info
    Update Billing Info
    Delete Account
    
    About to fall asleep, will add more functionality tomorrow morning

---
+++ OnTap/app/src/main/java/cs477/ontap/AccountDeletionActivity.java
@@ -0,0 +1,13 @@
+package cs477.ontap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AccountDeletionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_deletion);
+    }
+}
