d3ca3972d8f6b6d531cb94dce8e305c6671a1b81
Alex <ajs476@nau.edu>
2016-04-16 16:20:14 -0700

    Basic parse server get and put code
    testing getting and putting data into parse server

--- OnTap/app/src/main/java/cs477/ontap/HomeActivity.java
+++ OnTap/app/src/main/java/cs477/ontap/HomeActivity.java
@@ -2,20 +2,31 @@ package cs477.ontap;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.net.ParseException;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.parse.GetCallback;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 /**
  * Created by Alex on 4/15/2016.
  */
 public class HomeActivity extends AppCompatActivity {
+
+    public EditText locationTest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+        locationTest = (EditText)findViewById(R.id.editText_locationTest);
 
 
         final Dialog locationDialog = new Dialog(this);
@@ -35,6 +46,23 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //Toast.makeText(HomeActivity.this, "Location Selection Successful!", Toast.LENGTH_SHORT).show();
+                ParseQuery<ParseObject> query = ParseQuery.getQuery("Menu");
+                query.getInBackground("0toXQtPVHK", new GetCallback<ParseObject>() {
+                    @Override
+                    public void done(ParseObject object, com.parse.ParseException e) {
+                        if (e == null) {
+                            // object will be your game score
+                            String locationName = object.getString("locName");
+                            locationTest.setText(locationName);
+
+                        } else {
+                            // something went wrong
+
+                        }
+                    }
+
+
+                });
                 Intent myIntent = new Intent(HomeActivity.this, MyTabActivity.class);
                 startActivity(myIntent);
             }
